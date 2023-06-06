587c6bc14c7c47ab7ede8a3f019c283a18b57cb4
Michael Ouru <39490502+michaelouru-sw@users.noreply.github.com>
2023-06-07 02:05:10 +0300

    Modify the Header and hero components

--- components/testimonials.tsx
+++ components/testimonials.tsx
@@ -1,76 +1,161 @@
-import Image from 'next/image'
+import Image from "next/image";
 
-import TestimonialImage01 from '@/public/images/testimonial-01.jpg'
-import TestimonialImage02 from '@/public/images/testimonial-02.jpg'
-import TestimonialImage03 from '@/public/images/testimonial-03.jpg'
+import TestimonialImage01 from "@/public/images/testimonial-01.jpg";
+import TestimonialImage02 from "@/public/images/testimonial-02.jpg";
+import TestimonialImage03 from "@/public/images/testimonial-03.jpg";
 
 export default function Testimonials() {
   return (
     <section>
       <div className="max-w-6xl mx-auto px-4 sm:px-6">
         <div className="py-12 md:py-20 border-t border-gray-800">
-
           {/* Section header */}
           <div className="max-w-3xl mx-auto text-center pb-12 md:pb-20">
             <h2 className="h2 mb-4">Don't take our word for it</h2>
-            <p className="text-xl text-gray-400">Vitae aliquet nec ullamcorper sit amet risus nullam eget felis semper quis lectus nulla at volutpat diam ut venenatis tellus—in ornare.</p>
+            <p className="text-xl text-gray-400">
+              Vitae aliquet nec ullamcorper sit amet risus nullam eget felis
+              semper quis lectus nulla at volutpat diam ut venenatis tellus—in
+              ornare.
+            </p>
           </div>
 
           {/* Testimonials */}
           <div className="max-w-sm mx-auto grid gap-8 lg:grid-cols-3 lg:gap-6 items-start lg:max-w-none">
-
             {/* 1st testimonial */}
-            <div className="flex flex-col h-full p-6 bg-gray-800" data-aos="fade-up">
+            <div
+              className="flex flex-col h-full p-6 bg-gray-800"
+              data-aos="fade-up"
+            >
               <div>
                 <div className="relative inline-flex flex-col mb-4">
-                  <Image className="rounded-full" src={TestimonialImage01} width={48} height={48} alt="Testimonial 01" />
-                  <svg className="absolute top-0 right-0 -mr-3 w-6 h-5 fill-current text-purple-600" viewBox="0 0 24 20" xmlns="http://www.w3.org/2000/svg">
+                  <Image
+                    className="rounded-full"
+                    src={TestimonialImage01}
+                    width={48}
+                    height={48}
+                    alt="Testimonial 01"
+                  />
+                  <svg
+                    className="absolute top-0 right-0 -mr-3 w-6 h-5 fill-current text-purple-600"
+                    viewBox="0 0 24 20"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
                     <path d="M0 13.517c0-2.346.611-4.774 1.833-7.283C3.056 3.726 4.733 1.648 6.865 0L11 2.696C9.726 4.393 8.777 6.109 8.152 7.844c-.624 1.735-.936 3.589-.936 5.56v4.644H0v-4.531zm13 0c0-2.346.611-4.774 1.833-7.283 1.223-2.508 2.9-4.586 5.032-6.234L24 2.696c-1.274 1.697-2.223 3.413-2.848 5.148-.624 1.735-.936 3.589-.936 5.56v4.644H13v-4.531z" />
                   </svg>
                 </div>
               </div>
-              <blockquote className="text-lg text-gray-400 grow">— Open PRO lets me quickly get the insights I care about so that I can focus on my productive work. I've had Open PRO for about 24 hours now and I honestly don't know how I functioned without it before.</blockquote>
+              <blockquote className="text-lg text-gray-400 grow">
+                — "I've been using Lexa's software for the past year, and I've
+                been really impressed with the results. The software has helped
+                me to streamline my workflow, save time, and improve my
+                productivity. I would highly recommend Lexa Technologies to
+                anyone looking for a reliable and effective software solution."
+              </blockquote>
               <div className="text-gray-700 font-medium mt-6 pt-5 border-t border-gray-700">
-                <cite className="text-gray-200 not-italic">Anastasia Dan</cite> - <a className="text-purple-600 hover:text-gray-200 transition duration-150 ease-in-out" href="#0">UX Board</a>
+                <cite className="text-gray-200 not-italic">Anastasia Dan</cite>{" "}
+                -{" "}
+                <a
+                  className="text-purple-600 hover:text-gray-200 transition duration-150 ease-in-out"
+                  href="#0"
+                >
+                  HR Manager
+                </a>
               </div>
             </div>
 
             {/* 2nd testimonial */}
-            <div className="flex flex-col h-full p-6 bg-gray-800" data-aos="fade-up" data-aos-delay="200">
+            <div
+              className="flex flex-col h-full p-6 bg-gray-800"
+              data-aos="fade-up"
+              data-aos-delay="200"
+            >
               <div>
                 <div className="relative inline-flex flex-col mb-4">
-                  <Image className="rounded-full" src={TestimonialImage02} width={48} height={48} alt="Testimonial 02" />
-                  <svg className="absolute top-0 right-0 -mr-3 w-6 h-5 fill-current text-purple-600" viewBox="0 0 24 20" xmlns="http://www.w3.org/2000/svg">
+                  <Image
+                    className="rounded-full"
+                    src={TestimonialImage02}
+                    width={48}
+                    height={48}
+                    alt="Testimonial 02"
+                  />
+                  <svg
+                    className="absolute top-0 right-0 -mr-3 w-6 h-5 fill-current text-purple-600"
+                    viewBox="0 0 24 20"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
                     <path d="M0 13.517c0-2.346.611-4.774 1.833-7.283C3.056 3.726 4.733 1.648 6.865 0L11 2.696C9.726 4.393 8.777 6.109 8.152 7.844c-.624 1.735-.936 3.589-.936 5.56v4.644H0v-4.531zm13 0c0-2.346.611-4.774 1.833-7.283 1.223-2.508 2.9-4.586 5.032-6.234L24 2.696c-1.274 1.697-2.223 3.413-2.848 5.148-.624 1.735-.936 3.589-.936 5.56v4.644H13v-4.531z" />
                   </svg>
                 </div>
               </div>
-              <blockquote className="text-lg text-gray-400 grow">— Open PRO lets me quickly get the insights I care about so that I can focus on my productive work. I've had Open PRO for about 24 hours now and I honestly don't know how I functioned without it before.</blockquote>
+              <blockquote className="text-lg text-gray-400 grow">
+                — I had the pleasure of working with Lexa Technologies on a
+                recent project, and I was very impressed with their project
+                management and quality. The team was always available to answer
+                questions and provide updates, and they went above and beyond to
+                ensure that the project was completed on time and to the highest
+                standards. I would highly recommend Lexa Technologies to anyone
+                looking for a reliable and professional partner.
+              </blockquote>
               <div className="text-gray-700 font-medium mt-6 pt-5 border-t border-gray-700">
-                <cite className="text-gray-200 not-italic">Anastasia Dan</cite> - <a className="text-purple-600 hover:text-gray-200 transition duration-150 ease-in-out" href="#0">UX Board</a>
+                <cite className="text-gray-200 not-italic">Anastasia Dan</cite>{" "}
+                -{" "}
+                <a
+                  className="text-purple-600 hover:text-gray-200 transition duration-150 ease-in-out"
+                  href="#0"
+                >
+                  CEO
+                </a>
               </div>
             </div>
 
             {/* 3rd testimonial */}
-            <div className="flex flex-col h-full p-6 bg-gray-800" data-aos="fade-up" data-aos-delay="400">
+            <div
+              className="flex flex-col h-full p-6 bg-gray-800"
+              data-aos="fade-up"
+              data-aos-delay="400"
+            >
               <div>
                 <div className="relative inline-flex flex-col mb-4">
-                  <Image className="rounded-full" src={TestimonialImage03} width={48} height={48} alt="Testimonial 03" />
-                  <svg className="absolute top-0 right-0 -mr-3 w-6 h-5 fill-current text-purple-600" viewBox="0 0 24 20" xmlns="http://www.w3.org/2000/svg">
+                  <Image
+                    className="rounded-full"
+                    src={TestimonialImage03}
+                    width={48}
+                    height={48}
+                    alt="Testimonial 03"
+                  />
+                  <svg
+                    className="absolute top-0 right-0 -mr-3 w-6 h-5 fill-current text-purple-600"
+                    viewBox="0 0 24 20"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
                     <path d="M0 13.517c0-2.346.611-4.774 1.833-7.283C3.056 3.726 4.733 1.648 6.865 0L11 2.696C9.726 4.393 8.777 6.109 8.152 7.844c-.624 1.735-.936 3.589-.936 5.56v4.644H0v-4.531zm13 0c0-2.346.611-4.774 1.833-7.283 1.223-2.508 2.9-4.586 5.032-6.234L24 2.696c-1.274 1.697-2.223 3.413-2.848 5.148-.624 1.735-.936 3.589-.936 5.56v4.644H13v-4.531z" />
                   </svg>
                 </div>
               </div>
-              <blockquote className="text-lg text-gray-400 grow">— Open PRO lets me quickly get the insights I care about so that I can focus on my productive work. I've had Open PRO for about 24 hours now and I honestly don't know how I functioned without it before.</blockquote>
+              <blockquote className="text-lg text-gray-400 grow">
+                — I've been a client of Lexa Technologies for several years now,
+                and I've always been happy with their services. They are always
+                professional, reliable, and they demonstrate commitment to meet
+                my needs. I recently had a project that was due very quickly,
+                and Lexa Technologies worked tirelessly to get it done on time.
+                I was very impressed with their dedication and commitment to my
+                success. I would highly recommend Lexa Technologies to anyone
+                looking for a reliable and trustworthy partner.
+              </blockquote>
               <div className="text-gray-700 font-medium mt-6 pt-5 border-t border-gray-700">
-                <cite className="text-gray-200 not-italic">Anastasia Dan</cite> - <a className="text-purple-600 hover:text-gray-200 transition duration-150 ease-in-out" href="#0">UX Board</a>
+                <cite className="text-gray-200 not-italic">Anastasia Dan</cite>{" "}
+                -{" "}
+                <a
+                  className="text-purple-600 hover:text-gray-200 transition duration-150 ease-in-out"
+                  href="#0"
+                >
+                  Finance Manager
+                </a>
               </div>
             </div>
-
           </div>
-
         </div>
       </div>
     </section>
-  )
+  );
 }
